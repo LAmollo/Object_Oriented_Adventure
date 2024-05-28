aa95e1a072c0ea158507be8cc1adb69697439aa7
LAmollo <lawinyamuga@gmail.com>
2024-05-28 00:36:21 -0700

    adv

--- index.mjs
+++ index.mjs
@@ -29,3 +29,16 @@ const adventurer = {
   adventurer.roll();
   adventurer.roll();
   
+  class Character {
+    constructor(name) {
+      this.name = name;
+      this.health = 100; // standardized to a maximum of 100
+      this.inventory = [];
+    }
+  
+    roll(mod = 0) {
+      const result = Math.floor(Math.random() * 20) + 1 + mod;
+      console.log(`${this.name} rolled a ${result}.`);
+    }
+  }
+  
