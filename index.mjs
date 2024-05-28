c79faacd4ab7599c63c9d0eaf5dcd7773288d4f8
LAmollo <lawinyamuga@gmail.com>
2024-05-28 00:38:08 -0700

    char...

--- index.mjs
+++ index.mjs
@@ -42,3 +42,10 @@ const adventurer = {
     }
   }
   
+const robin = new Character("Robin");
+robin.inventory = ["sword", "potion", "artifact"];
+robin.companion = new Character("Leo");
+robin.companion.type = "Cat";
+robin.companion.companion = new Character("Frank");
+robin.companion.companion.type = "Flea";
+robin.companion.companion.inventory = ["small hat", "sunglasses"];
